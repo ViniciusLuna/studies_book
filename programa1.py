090b8ddec7a50a99a8aaded42222fd0c347a5f88
Vinicius Luna <vini.viniciusluna@me.com>
2023-09-18 22:44:53 -0300

    Adcionado em tipos de dados cap2, variaveis, operadores relacionais e logicos

---
+++ programa1.py
@@ -0,0 +1,27 @@
+#Capitulo 02 Tipos de Dados
+
+#Python nao exige a declaracao de variaveis antes de seu uso - autor
+
+pi = 3.14159265
+raio = 5
+altura =10
+vol_cil = pi * raio **2 * altura
+print(vol_cil)
+
+#Operadores relacionais
+idade = 18
+idade >= 18
+#True
+idade < 18
+#False
+idade == 18
+#True
+idade != 18
+#False
+
+#Quando duas ou mais expressoes precisam ser avaliadas para que o resultado possa ser utilizado na tomada de decisao, devemos nos valer dos operadores logicos - autor 
+#Operadores logicos
+idade >= 16 and idade <= 21
+#True
+
+#Matrizes
