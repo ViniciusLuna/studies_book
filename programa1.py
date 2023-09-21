6ee8090c849730e1a63b98bdbfe60617808dd9cc
Vinicius Luna <vini.viniciusluna@me.com>
2023-09-20 23:44:47 -0300

    Feito alguns exemplos de tipos de dados do livro

--- programa1.py
+++ programa1.py
@@ -6,7 +6,7 @@ pi = 3.14159265
 raio = 5
 altura =10
 vol_cil = pi * raio **2 * altura
-print(vol_cil)
+
 
 #Operadores relacionais
 idade = 18
@@ -25,3 +25,38 @@ idade >= 16 and idade <= 21
 #True
 
 #Matrizes
+dia_mes = [31,28,31,30,31,30,31,31,30,31,30,31]
+#Forma dinamica do seu array
+quantidade = int(input("Informe a quantidade de valors:"))
+valores = [0 for x in range(quantidade)]
+valores[4] = 520
+print(valores)
+
+#As matrizes (ou listas) em Python sao, na verdade, uma classe para armazenamento de dados. 
+#Alguns metodos para insercao e remocao de elementos de uma matriz
+valores = [100,320,450,560,680]
+valores.insert(3,280)
+print(valores)
+valores.append(800)
+print(valores)
+
+#if 450 in valores: print (True)
+450 in valores
+#True
+
+
+tamanho = 5
+medicoes = [0 for x in range(tamanho)]
+consumo = float(input("Informe a 1a. medição de consumo:"))
+total = consumo
+consumo = float(input("Informe a 2a. medição de consumo:"))
+total+= consumo
+consumo = float(input("Informe a 3a. medição de consumo:"))
+total+= consumo
+consumo = float(input("Informe a 4a. medição de consumo:"))
+total+= consumo
+consumo = float(input("Informe a 5a. medição de consumo:"))
+total+= consumo
+
+media = total / tamanho
+print("Média das 5 medições de consumo %8.2f"% media) #8 digitos inteiros e 2 casas decimais
